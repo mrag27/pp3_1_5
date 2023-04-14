58adb9a789ef06e40500f46a4e60079a7704c5f0
Bulgakov Egor <egor_mrag1999@,ail.ru>
2023-04-14 17:51:54 +0300

    ispr

--- src/main/java/ru/kata/spring/boot_security/demo/contollers/ViewController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/contollers/ViewController.java
@@ -3,13 +3,11 @@ package ru.kata.spring.boot_security.demo.contollers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.kata.spring.boot_security.demo.models.User;
 
 @Controller
 public class ViewController {
     @GetMapping("/admin")
     public String showAllUser(Model model) {
-        model.addAttribute("newUser", new User());
         return "admin-profile";
     }
 
